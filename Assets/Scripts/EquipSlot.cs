e6ca95e7278572097cf3b799651a1d8f41f65a23
quattroro <kio7147@naver.com>
2022-06-08 23:55:47 +0900

    1234

---
+++ Assets/Scripts/EquipSlot.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipSlot : BaseSlot
+{
+    public override void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
